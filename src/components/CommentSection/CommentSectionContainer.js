a5280ea9688ef0c328a59417cdacde41d76952ce
Eric Taylor <erictaylor@Erics-MacBook-Pro.local>
2020-04-14 21:52:08 -0400

    Project working - Like Button ONLY INCREASES

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -6,10 +6,21 @@ import "./Comment.css";
 
 const CommentSection = props => {
   // Add state for the comments
-
+  console.log(props);
+  const [comments, setComments] = useState(props.comments);
+  
+  
+  
   return (
     <div>
-      {/* map through the comments data and return the Comment component */}
+      {/* map through the comments data and return the Comment component */
+        comments.map((item, index) =>{
+          return <Comment key={index} comment={item}/>
+        })
+      }
+      
+      
+
       <CommentInput />
     </div>
   );
